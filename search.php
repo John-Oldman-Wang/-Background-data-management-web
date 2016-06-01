5a99b44d22c841b2f90683a740c8ae788ad6db51
wanglin <mag1c_shen_09@foxmail.com>
2016-06-02 01:11:01 +0800

    完成加载页面用户类型的跟新测试，已经查找的AJAX

---
+++ search.php
@@ -0,0 +1,5 @@
+<?php
+header("Content-Type:applocation/json;charset=utf-8");
+echo '{"success":true,"id": ["101","102","103"],"name":["jack","tony","sundy"],"remark":["taobao11","jd11","other"],"status":["在线","不在线","在线"],"createtime":["2012111","20131212","20161220"],"logintime":["20160530","20160520","20160601"],"type": ["淘宝用户","京东用户","非网店用户"]}';
+
+?>
---
+++ usertype.php
@@ -0,0 +1,4 @@
+<?php
+header("Content-Type:applocation/json;charset=utf-8");
+echo '{"success":true,"type": ["淘宝用户","京东用户","非网店用户"]}';
+?>
